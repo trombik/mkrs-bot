28f47a5739215e1081710dfaee61730a30e82c56
Tomoyuki Sakurai <y@trombik.org>
2025-03-19 16:25:49 +0700

    feat: introduce lookbook

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  if Rails.env.development?
+    mount Lookbook::Engine, at: "/lookbook"
+  end
 end
