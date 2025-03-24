4a83a709f2d9c95ebfaba13503176cefec69e409
Tomoyuki Sakurai <y@trombik.org>
2025-03-24 15:48:14 +0700

    feat: import bootstrap-icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
+
+# Add bootstrap-icons
+Rails.application.config.assets.paths << Rails.root.join("vendor", "icon", "bootstrap-icons", "font", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "icon", "bootstrap-icons", "font")
