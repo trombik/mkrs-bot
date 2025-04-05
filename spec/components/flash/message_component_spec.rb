766438fed65feddadec51b7a38aa4e31de57b067
Tomoyuki Sakurai <y@trombik.org>
2025-04-05 09:35:33 +0700

    feat: support dismissible button in flash message

--- spec/components/flash/message_component_spec.rb
+++ spec/components/flash/message_component_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe Flash::MessageComponent, type: :component do
   it "renders info flash message" do
     render_inline(described_class.new(type: :notice)) { text }
 
-    expect(page).to have_css "div div", text: text
+    expect(page).to have_css "div", text: text
+  end
+
+  context "when dismissible is true" do
+    it "renders a button to dismiss" do
+      render_inline(described_class.new(type: :notice, dismissible: true)) { text }
+
+      expect(page).to have_button
+    end
+  end
+
+  context "when dismissible is false" do
+    it "renders a button to dismiss" do
+      render_inline(described_class.new(type: :notice, dismissible: false)) { text }
+
+      expect(page).not_to have_button
+    end
   end
 end
--- spec/components/previews/flash/message_component_preview.rb
+++ spec/components/previews/flash/message_component_preview.rb
@@ -13,6 +13,12 @@ class Flash::MessageComponentPreview < ViewComponent::Preview
     end
   end
 
+  def dismissible_notice
+    render(Flash::MessageComponent.new(type: :notice, dismissible: true)) do
+      "Dismessible Notice"
+    end
+  end
+
   def alert
     render(Flash::MessageComponent.new(type: :alert)) do
       "This is alert"
@@ -35,6 +41,20 @@ class Flash::MessageComponentPreview < ViewComponent::Preview
       case, you pass your options as the second parameter. Set escape to false
         to disable escaping. Note: this is legacy syntax, see tag method
         description for details."
+    end
+  end
+
+  def dismissible_long_text
+    render(Flash::MessageComponent.new(type: :notice, dismissible: true)) do
+      text = tag.p "Returns an HTML block tag of type name surrounding the content. Add
+      HTML attributes by passing an attributes hash to options. Instead of
+      passing the content as an argument, you can also use a block in which
+      case, you pass your options as the second parameter. Set escape to false
+        to disable escaping. Note: this is legacy syntax, see tag method
+        description for details."
+      content = text
+      content += tag.hr
+      content += text
     end
   end
 end
