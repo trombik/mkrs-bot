849daf2d8fa7c21dadc5f8e41e51acfb9776c37a
Tomoyuki Sakurai <y@trombik.org>
2025-05-25 11:18:34 +0700

    Revert "feat: introduce StaffUser"
    This reverts commit 194f2d8df9ded33d4f16c3db1a17d65c214735be.

--- app/helpers/staff_users_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# A helper module for StaffUsers
-module StaffUsersHelper
-end
--- spec/factories/staff_users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :staff_user do
-    display_name { "Example user" }
-    account { "@foo" }
-    staff_group
-  end
-end
