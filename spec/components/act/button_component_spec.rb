872c1f9a0e1b8dd490a361ab6f3f5c95f6147b84
Tomoyuki Sakurai <y@trombik.org>
2025-06-29 05:39:01 +0700

    feat: support `disabled` in Act::ButtonComponent

--- spec/components/act/button_component_spec.rb
+++ spec/components/act/button_component_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe Act::ButtonComponent, type: :component do
   it "has link to the act" do
     expect(page).to have_link act.name, href: act_path(act)
   end
+
+  context "when the recoed is a new record" do
+    let(:act) { build(:act, task: create(:task, user: create(:user))) }
+
+    it "is disabled" do
+      expect(page).to have_css("a.disabled")
+    end
+
+    it "has a link to #" do
+      expect(page).to have_css("a[href='#']")
+    end
+  end
+
+  context "when the recoed is not a new record" do
+    it "is enabled" do
+      expect(page).to have_no_css("a.disabled")
+    end
+
+    it "has a link to act" do
+      expect(page).to have_css("a[href='#{act_path(act)}']")
+    end
+  end
 end
