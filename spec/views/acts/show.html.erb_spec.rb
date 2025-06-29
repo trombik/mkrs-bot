cb3714e59b1936141d9ead62d22ae906310bcf08
Tomoyuki Sakurai <y@trombik.org>
2025-06-29 15:17:54 +0700

    view: update acts/show to use Act::DetailCardComponent

--- spec/views/acts/show.html.erb_spec.rb
+++ spec/views/acts/show.html.erb_spec.rb
@@ -3,16 +3,26 @@
 require "rails_helper"
 
 RSpec.describe "acts/show", type: :view do
+  let(:act) { create(:act, task: create(:task, user: create(:user))) }
+
   before do
-    assign(:act, create(:act, task: create(:task, user: create(:user))))
+    assign(:act, act)
+    render
   end
 
-  # rubocop:disable RSpec/MultipleExpectations
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/Description/)
+  it "renders card for act" do
+    expect(rendered).to have_css("div#card_#{dom_id(act)}")
   end
-  # rubocop:enable RSpec/MultipleExpectations
+
+  it "has link to acts" do
+    expect(rendered).to have_link href: acts_path
+  end
+
+  it "has link to edit acts" do
+    expect(rendered).to have_link href: edit_act_path(act)
+  end
+
+  it "has a button to destroy the act" do
+    expect(rendered).to have_selector :link_or_button, "Destroy this act"
+  end
 end
